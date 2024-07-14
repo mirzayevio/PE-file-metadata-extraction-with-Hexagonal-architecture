6c20764ca0ae3b2c35cf6d8f84c7ef75ba22cb77
mirzayevio <mirzayevfarid@gmail.com>
2024-07-14 13:59:59 +0200

    refactor: remove unnecessary methods from adapters

--- src/adapters/repositories/metadata.py
+++ src/adapters/repositories/metadata.py
@@ -1,5 +1,4 @@
 from abc import ABC
-from typing import Optional
 
 from src.domain.entities.metadata import Metadata
 from src.domain.ports.repositories.metadata import MetadataRepositoryInterface
@@ -8,15 +7,11 @@ from src.domain.ports.repositories.metadata import MetadataRepositoryInterface
 class MetadataRepository(MetadataRepositoryInterface, ABC):
     """Postgresql Repository for Metadata"""
 
-    def __init__(self, session) -> None:
+    def __init__(self) -> None:
         super().__init__()
-        self.session = session
 
     def _add(self, metadata: Metadata) -> None:
         pass
 
     def _get_by_uuid(self, uuid: str) -> Metadata:
-        pass
-
-    def _get_all(self) -> Optional[list[Metadata]]:
         pass
--- src/adapters/services/metadata.py
+++ src/adapters/services/metadata.py
@@ -1,5 +1,4 @@
 from abc import ABC
-from typing import Any, Optional
 
 from src.domain.entities.metadata import Metadata
 from src.domain.ports.repositories.metadata import MetadataRepositoryInterface
@@ -11,12 +10,9 @@ class MetadataService(MetadataServiceInterface, ABC):
     def __init__(self, repository: MetadataRepositoryInterface) -> None:
         self.repository = repository
 
-    def _create(self, metadata: CreateMetadataInputDto) -> Optional[Metadata]:
+    def _create(self, metadata: CreateMetadataInputDto):
         metadata_dict = metadata.model_dump()
         self.repository.add(Metadata.from_dict(metadata_dict))
 
     def _get_metadata_by_uuid(self, uuid: str) -> Metadata:
-        pass
-
-    def _get_all_metadata(self) -> Optional[list[Any]]:
         pass
